b95c928d6c8d09e2d57d94545ebbfd516ad5ae97
Amar Premsaran Patel <amarppatel1@gmail.com>
2023-07-15 18:08:06 -0400

    Cleanup for DTO projects.

--- chatbotservicedto/README.md
+++
@@ -1,3 +0,0 @@
-# Build and Install Latest Saathratri TAJ Vote's DTO JAR to Local Maven Repository
-
-$ ./mvnw clean install
--- cleanup.sh
+++ cleanup.sh
@@ -3,15 +3,18 @@ echo "Deleting docker-compose..."
 rm -fr docker-compose 
 echo "Deleting Booking Service..."
 rm -fr bookingservice
+rm -fr bookingservicedto
 echo "Deleting Chatbot Service..."
 rm -fr chatbotservice
+rm -fr chatbotservicedto
 echo "Deleting Repair Service..."
 rm -fr repairservice
+rm -fr repairservicedto
 echo "Deleting Customer Service Service..."
 rm -fr customerservice
+rm -fr customerservicedto
 echo "Deleting Chatbot Gateway..."
 rm -fr chatbotgateway
-rm -fr node_modules
 echo "Deleting package.json..."
 rm package.json
 rm package-lock.json
@@ -22,3 +25,6 @@ echo "Deleting .jhipster..."
 rm -fr .jhipster
 echo "Deleting target..."
 rm -fr target
+echo "Deleting node_modules..."
+rm -fr node_modules
+
