51e493c5e43fa804b8cde324eb98247bcc55dcbe
Amar Premsaran Patel <amarppatel1@gmail.com>
2023-07-15 18:36:27 -0400

    Added DTO support

--- compile-saathratri-dev.sh
+++ compile-saathratri-dev.sh
@@ -1,5 +1,23 @@
 echo "Compiling domain projects, then checking them into Maven repository..."
 
+echo "Compile and Install all DTO JARs..."
+
+cd bookingservicedto
+./mvnw clean install
+cd ..
+
+cd chatbotservicedto
+./mvnw clean install
+cd ..
+
+cd repairservicedto
+./mvnw clean install
+cd ..
+
+cd customerservicedto
+./mvnw clean install
+cd ..
+
 echo "Comiling Chatbot Gateway"
 cd chatbotgateway
 ./mvnw clean package -Pdev,api-docs -DskipTests
@@ -10,17 +28,9 @@ cd bookingservice
 ./mvnw clean package -Pdev,api-docs -DskipTests
 cd ..
 
-cd bookingservicedto
-./mvnw clean install
-cd ..
-
 echo "Compiling Chatbot Microservice"
 cd chatbotservice
 ./mvnw clean package -Pdev,api-docs -DskipTests
-cd ..
-
-cd chatbotservicedto
-./mvnw clean install
 cd ..
 
 echo "Compiling Repair Microservice"
@@ -28,17 +38,9 @@ cd repairservice
 ./mvnw clean package -Pdev,api-docs -DskipTests
 cd ..
 
-cd repairservicedto
-./mvnw clean install
-cd ..
-
 echo "Compiling Customer Microservice"
 cd customerservice
 ./mvnw clean package -Pdev,api-docs -DskipTests
-cd ..
-
-cd customerservicedto
-./mvnw clean install
 cd ..
 
 echo "Completed compilation of Hotel Chatbot Development Version"
