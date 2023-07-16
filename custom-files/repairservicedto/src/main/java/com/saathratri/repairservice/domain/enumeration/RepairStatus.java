c0bf23de2e1e54a28c88a9339bb19aeaa3802b0e
Amar Premsaran Patel <amarppatel1@gmail.com>
2023-07-16 12:13:41 -0400

    Adding support for Enums in DTO JARs.

---
+++ custom-files/repairservicedto/src/main/java/com/saathratri/repairservice/domain/enumeration/RepairStatus.java
@@ -0,0 +1,10 @@
+package com.saathratri.repairservice.domain.enumeration;
+
+/**
+ * The RepairStatus enumeration.
+ */
+public enum RepairStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+}
